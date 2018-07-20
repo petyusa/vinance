cdb659f2450abef3f0895e53764f37a29b8e5192
petyusa <peter.vincze.dev@gmail.com>
2018-07-20 16:37:50 +0200

    added custom font style sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SidenavComponent],
   imports: [AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
