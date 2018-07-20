0bdd8fc801fc15e36d1009680f5dfb04f7a565c2
petyusa <peter.vincze.dev@gmail.com>
2018-07-20 10:25:24 +0200

    removed custom host

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
